17291aa70eb3993c8cbb6961f481afabd0971e20
Nish <nish_mandal@hotmail.com>
2022-02-28 11:13:39 +0000

    Operators part 1 lesson complete

--- Core/Operators_Solution/Operators/Program.cs
+++ Core/Operators_Solution/Operators/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Operators;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //int x = 5;
+        //int y = 5;
+
+        //int a = x++;
+        //int b = ++y;
+
+        //for (int i = 1; i <= 5; ++i)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        //var c = 5 / 2; // 2
+        //var d = 5.0 / 2; //2.5
+        //var e = 5 / 3; //1
+
+        //double f = 5 / 2;
+
+        //Sebs burthday is 23 days from now. Put it in the format of days and weeks 
+
+        //var daysInAWeek = 7;
+        //var todalDaysToBirthday = 23;
+        //var weeksToBirthday = todalDaysToBirthday / daysInAWeek;
+        //var days = todalDaysToBirthday % daysInAWeek;
+        //Console.WriteLine("Weeks: " + weeksToBirthday + " Days: " + days);
+    }
+
+    public static bool EvenOff(int num)
+    {
+        //Will reture true if num is even, false if it is odd
+        return true;
+    }
+
+    public static int FinSumDiv3And5(int n)
+    {
+        //it will add up all numbers between 1 and n inclusive
+        //And return this sum
+
+        return -1;
+    }
+}
