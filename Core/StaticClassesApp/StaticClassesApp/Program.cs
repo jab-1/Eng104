c213cef77a597de1c9a6fb527cbb45d6769b55f5
Nish <nish_mandal@hotmail.com>
2022-03-01 11:02:11 +0000

    static methods and class example

---
+++ Core/StaticClassesApp/StaticClassesApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace StaticClassesApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Trainee.GetNumberOfTrainees());
+        var nishMan = new Trainee(30)
+        {
+            Course = "C# Dev",
+            Name = "Nish Mandal",
+        };
+
+        var nishKumar = new Trainee(35)
+        {
+            Course = "Java Dev",
+            Name = "Nish Kumar",
+        };
+
+        Console.WriteLine(nishMan.Name + " " + nishMan.Age + " " + nishMan.Course);
+        Console.WriteLine(nishKumar.Name + " " + nishKumar.Age + " " + nishKumar.Course);
+        Console.WriteLine(nishMan.SayHello());
+        Console.WriteLine(Trainee.GetNumberOfTrainees());
+
+    }
+}
+
+
+public class Trainee
+{
+    public string Course { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    private static int _noOfTrainees = 0;
+
+    public Trainee(int age)
+    {
+        Age = age;
+        _noOfTrainees++;
+    }
+
+    public static int GetNumberOfTrainees()
+    {
+        return _noOfTrainees;
+    }
+
+    public string SayHello()
+    {
+        return "Hello " + Name;
+    }
+}
+
+
+public static class Calculator
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
